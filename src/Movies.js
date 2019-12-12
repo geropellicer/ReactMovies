175188162434bd895c21057a1f5a059ce611af19
Gero <geronimo@devsar.com>
2019-12-12 12:36:43 -0300

    Movies cards styling done

--- src/Movies.js
+++ src/Movies.js
@@ -4,10 +4,14 @@ const Movie = ({title, img, id, price, description}) => {
     return(
         <div className="movie">
             <img src={img} alt={title}/>
-            <h1>{title}</h1>
-            <h2>{price}</h2>
-            <h3>{id}</h3>
-            <p>{description}</p>
+            <div className="movie-data">
+                <h1>{title}</h1>
+                <div className="movie-row">
+                    <span>Price:&nbsp;</span><h2>${price}</h2>
+                    <span>ID:&nbsp;</span><h3>{id}</h3>
+                </div>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
