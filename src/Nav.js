4e8934008ea3de4ec1617ceef376704a7bd6c09b
Gero <geronimo@devsar.com>
2019-12-12 12:10:49 -0300

    Nav styling finished

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import { MovieContext } from './MovieContext';
 
 const Nav = () => {
+
+    const [movies, setMovies] = useContext(MovieContext);
+
     return(
         <div className="nav">
-            <h1>Movies</h1>
+            <div className="site-title">
+                <h1>Movie List</h1>
+                <span>Incorporated limited</span>
+            </div>
+            
+            <div className="site-catalog-state">
+                <h6>Movies in catalog: </h6>
+                <span>&nbsp;{movies.length}</span>
+            </div>
         </div>
     );
 };
